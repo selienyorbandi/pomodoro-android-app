2c2a887f530c8d71df379c19badcf75aa27057e9
Selien Yorbandi <selienyorbandi@gmail.com>
2023-04-30 15:42:48 -0300

    Feat: upload pomodoro basic Android APP

---
+++ app/src/main/java/com/example/pomodoroapp/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.pomodoroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CountDownTimer timer;
+    private TextView timerTextView;
+    private ImageView startButton, stopButton;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerTextView = findViewById(R.id.timerTextView);
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopTimer();
+            }
+        });
+    }
+
+    private void startTimer() {
+        startButton.setEnabled(false);
+        stopButton.setEnabled(true);
+
+        timer = new CountDownTimer(25 * 60 * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int minutes = (int) (millisUntilFinished / 1000) / 60;
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+                timerTextView.setText(timeLeftFormatted);
+            }
+
+            @Override
+            public void onFinish() {
+                timerTextView.setText("00:00");
+                startButton.setEnabled(true);
+                stopButton.setEnabled(false);
+            }
+        };
+
+        timer.start();
+    }
+
+    private void stopTimer() {
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
+        timer.cancel();
+        timerTextView.setText("25:00");
+    }
+}
